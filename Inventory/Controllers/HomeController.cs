160284a97aa7eb30dd448a246973a10e80e6e435
Prakash Arulanantham <eee_prakash@rediffmail.com>
2015-09-24 18:39:23 +0000

    Build Error

--- Inventory/Controllers/HomeController.cs
+++ Inventory/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Inventory.Controllers
         {
             ViewBag.Message = "Your application description page.";
 
-         //   return View();
+           return View();
         }
 
         public ActionResult Contact()
