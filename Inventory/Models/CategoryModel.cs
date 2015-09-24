7fed6dc87607230367b7facf9f9224cba1c51533
Prakash Arulanantham <eee_prakash@rediffmail.com>
2015-09-24 14:27:46 +0000

    Added Category Model

---
+++ Inventory/Models/CategoryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+namespace Inventory.Models
+{
+    public class CategoryModel
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        
+
+        public int CategoryModelId { get; set; }
+        public string CategoryModelName { get; set; }
+
+        
+    }
+}
